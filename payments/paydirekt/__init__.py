fc8355d5768d6648b5fae0510de2f12431795f87
alex <devkral@web.de>
2017-06-15 23:25:55 +0200

    fixes for paydirekt

--- payments/paydirekt/__init__.py
+++ payments/paydirekt/__init__.py
@@ -1,3 +1,6 @@
+""" paydirekt payment provider """
+
+
 from __future__ import unicode_literals
 try:
     # For Python 3.0 and later
@@ -7,17 +10,22 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import URLError
     from urllib import urlencode
-from django.http import HttpResponseRedirect
 
-from .. import PaymentError, PaymentStatus, RedirectNeeded
-from ..core import BasicProvider
-import requests
 import uuid
-import datetime
-import base64
+from datetime import datetime, timezone, timedelta
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 import os
 import email.utils
 import hmac
+import json
+import time
+
+import requests
+from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
+from django.conf import settings
+
+from .. import PaymentError, PaymentStatus, RedirectNeeded
+from ..core import BasicProvider
 
 
 # Capture: if False ORDER is used
@@ -28,15 +36,19 @@ class PaydirektProvider(BasicProvider):
     api_key:
         seller key, assigned by paydirekt
     secret:
-        seller secret key
+        seller secret key (=encoded in base64)
     endpoint:
         which endpoint to use
     '''
-    token_raw = None
-    token_uuid = None
     access_token = None
     expires_in = None
-    api_version = None
+
+    path_token = "{}/api/merchantintegration/v1/token/obtain"
+    path_checkout = "{}/api/checkout/v1/checkouts"
+    path_capture = "{}/api/checkout/v1/checkouts/{}/captures"
+    path_release = "{}/api/checkout/v1/checkouts/{}/close"
+    path_refund = "{}/api/checkout/v1/checkouts/{}/refunds"
+
 
     translate_status = {
         "APPROVED": PaymentStatus.CONFIRMED,
@@ -52,77 +64,89 @@ class PaydirektProvider(BasicProvider):
     }
 
 
-    def __init__(self, api_key, secret, endpoint="https://api.sandbox.paydirekt.de", overcapture=False, api_version="v1", **kwargs):
+    def __init__(self, api_key, secret, endpoint="https://api.sandbox.paydirekt.de", \
+                 overcapture=False, **kwargs):
+        if not isinstance(secret, bytes):
+            self.secret_b64 = secret.encode('utf-8')
         self.api_key = api_key
-        self.secret = secret
         self.endpoint = endpoint
-        self.api_version = api_version
         self.overcapture = overcapture
         super(PaydirektProvider, self).__init__(**kwargs)
 
     def retrieve_oauth_token(self):
-        self.token_uuid = uuid.uuid4()
-        nonce = base64.urlsafe_b64encode(os.urandom(64))
-        date = datetime.datetime.utcnow()
-        strsign = self.token_uuid+date.strftime("%Y%m%d%H%M%S")+self.api_key+nonce
-        h = hmac.new(strsign, msg=base64.urlsafe_b64encode(self.secret.encode("utf-8")), digestmod='sha256')
+        """ Retrieves oauth Token and save it as instance variable """
+        token_uuid = str(uuid.uuid4()).encode("utf-8")
+        nonce = urlsafe_b64encode(os.urandom(48))
+        date_now = datetime.now(timezone.utc)
+        bytessign = token_uuid+b":"+date_now.strftime("%Y%m%d%H%M%S").encode('utf-8')+b":"+self.api_key.encode('utf-8')+b":"+nonce
+        h_temp = hmac.new(urlsafe_b64decode(self.secret_b64), msg=bytessign, digestmod='sha256')
 
         header = PaydirektProvider.header_default.copy()
-        header.update(("X-Auth-Key", self.api_key))
-        header.update(("X-Request-ID", self.token_uuid))
+        header["X-Auth-Key"] = self.api_key
+        header["X-Request-ID"] = token_uuid
 
-        header.update(("X-Auth-Code", base64.urlsafe_b64encode(h.digest())))
-        header.update(("Date", email.utils.format_datetime(date, usegmt=True))
+        header["X-Auth-Code"] = str(urlsafe_b64encode(h_temp.digest()), 'ascii')
+        header["Date"] = email.utils.format_datetime(date_now, usegmt=True)
         body = {
             "grantType" : "api_key",
-            "randomNonce" : nonce
+            "randomNonce" : str(nonce, "ascii")
         }
-        response = request.post(f"/api/merchantintegration/{self.api_version}/token/obtain", data=body, header=header)
-        response.raise_for_status()
-        self.token_raw = response.json()
-        self.access_token = self.token_raw["access_token"]
-        self.expires_in = datetime.fromtimestamp(self.token_raw["expires_in"], datetime.timezone.utc)
+        response = requests.post(self.path_token.format(self.endpoint), json=body, headers=header)
+        token_raw = response.json()
+        if response.status_code != 200:
+            self.token_raw = token_raw
+            error_code = token_raw["messages"][0]["code"] if len(token_raw["messages"]) > 0 else None
+            gateway_error = token_raw.get("error_description", None)
+            raise PaymentError(str(response.status_code), code=error_code, gateway_message=gateway_error)
+        self.access_token = token_raw["access_token"]
+        self.expires_in = date_now+timedelta(seconds=token_raw["expires_in"])
+        if getattr(settings, "DEBUG", False):
+            self.token_raw = token_raw
+            self.token_uuid = token_uuid
 
     def check_and_update_token(self):
-        if not self.expires_in or self.expires_in >= datetime.datetime.now(datetime.timezone.utc):
+        """ Check if token exists or has expired, renew it in this case """
+        if not self.expires_in or self.expires_in >= datetime.now(timezone.utc):
             self.retrieve_oauth_token()
 
     def get_form(self, payment, data=None):
         if not payment.id:
             payment.save()
         self.check_and_update_token()
-        header = PaydirektProvider.header_default.copy()
-        header.update(("Authorization", f"Bearer {self.access_token}"))
-
+        headers = PaydirektProvider.header_default.copy()
+        headers["Authorization"] = "Bearer %s" % self.access_token
+        # email_hash = sha256(payment.billing_email.encode("utf-8")).digest())
         body = {
-            "type": getattr(payment, "saletype", "DIRECT_SALE"),
+            "type": "ORDER" if not self._capture else "DIRECT_SALE",
             "totalAmount": payment.total,
             "shippingAmount": payment.delivery,
-            #"orderAmount": getattr(payment, "order", None),
+            "orderAmount": payment.total - payment.delivery,
             "currency": payment.currency,
             #"items": getattr(payment, "items", None),
             #"shoppingCartType": getattr(payment, "carttype", None),
-            "deliveryType": "ORDER" if not self._capture else "DIRECT_SALE",
-            "merchantOrderReferenceNumber": str(payment.id),
+            #"deliveryType": getattr(payment, "deliverytype", None),
+            # payment id can repeat if different shop systems are used
+            "merchantOrderReferenceNumber": "%s:%s" % (hex(int(time.time())), payment.id),
             "redirectUrlAfterSuccess": payment.get_success_url(),
             "redirectUrlAfterCancellation": payment.get_failure_url(),
             "redirectUrlAfterRejection": payment.get_failure_url(),
             "callbackUrlStatusUpdates": self.get_return_url(payment),
-            #"sha256hashedEmailAddress": base64.urlsafe_b64encode(hashlib.sha256(payment.billing_email.encode("utf-8")).digest()),
+            #"sha256hashedEmailAddress": str(urlsafe_b64encode(email_hash), 'ascii'),
             "minimumAge": getattr(payment, "minimumage", None),
             "redirectUrlAfterAgeVerificationFailure": payment.get_failure_url(),
             #"note": payment.message[0:37]
 
         }
         if self.overcapture and body["type"] == "ORDER":
-            body.update(("overcapture", True))
+            body["overcapture"] = True
+
         shipping = {
             "addresseeGivenName": payment.billing_first_name,
             "addresseeLastName": payment.billing_last_name,
             "company": getattr(payment, "billing_company", None),
-            #"additionalAddressInformation":,
-            "street": payment.billing_address_1,
-            "streetNr": payment.billing_address_2,
+            "additionalAddressInformation": payment.billing_address_2,
+            "street": payment.billing_address_1.rsplit(" ", 1)[0],
+            "streetNr": payment.billing_address_1.rsplit(" ", 1)[1],
             "zip": payment.billing_postcode,
             "city": payment.billing_city,
             "countryCode": payment.billing_country_code,
@@ -134,10 +158,10 @@ class PaydirektProvider(BasicProvider):
 
         body["shippingAddress"] = shipping
 
-        response = request.post(f"/api/merchantintegration/{self.api_version}/token/obtain", data=body, header=header)
+        response = requests.post(self.path_checkout.format(self.endpoint), json=body, headers=headers)
         response.raise_for_status()
-        ob = response.json()
-        raise RedirectNeeded(ob["approve"])
+        json_response = response.json()
+        raise RedirectNeeded(json_response["_links"]["approve"]["href"])
 
     def process_data(self, payment, request):
         if request.status != 200:
@@ -146,39 +170,47 @@ class PaydirektProvider(BasicProvider):
             results = json.loads(request.body)
         except (ValueError, TypeError):
             return HttpResponseForbidden('FAILED')
-        if response.reasonCode != "APPROVED" and not payment.transaction_id:
+        if results.reasonCode != "APPROVED" and not payment.transaction_id:
             payment.remove()
         else:
-            if response.reasonCode == "APPROVED":
+            if results.reasonCode == "APPROVED":
                 payment.transaction_id = results["checkoutId"]
                 if self._capture:
-                    payment.change_status(translate_status(results["checkoutStatus"]))
+                    payment.change_status(self.translate_status[results["checkoutStatus"]])
                 else:
                     payment.change_status(PaymentStatus.PREAUTH)
             else:
-                payment.change_status(translate_status(results["checkoutStatus"], self._capture))
+                payment.change_status(self.translate_status[results["checkoutStatus"]])
             payment.save()
+        return HttpResponse('OK')
+
+    def get_hidden_fields(self, payment):
+        return []
+    def get_token_from_request(self, payment, request):
+        return ""
 
     def capture(self, payment, amount=None):
         if not amount:
             amount = payment.total
         self.check_and_update_token()
         header = PaydirektProvider.header_default.copy()
-        header.update(("Authorization", f"Bearer {self.access_token}"))
+        header["Authorization"] = f"Bearer {self.access_token}"
         body = {
             "amount": amount,
-            "callbackUrlStatusUpdates", self.get_return_url(payment)
+            "callbackUrlStatusUpdates": self.get_return_url(payment)
         }
-        response = request.post(f"/api/checkout/{self.api_version}/checkouts/{payment.transaction_id}/captures", data=body, header=header)
+        response = requests.post(self.path_capture.format(self.endpoint, payment.transaction_id), \
+                                 json=body, headers=header)
         response.raise_for_status()
-        ob = response.json()
-        return ob["amount"]
+        json_response = response.json()
+        return json_response["amount"]
 
     def release(self, payment):
         self.check_and_update_token()
         header = PaydirektProvider.header_default.copy()
-        header.update(("Authorization", f"Bearer {self.access_token}"))
-        response = request.post(f"/api/checkout/{self.api_version}/checkouts/{payment.transaction_id}/captures/close", header=header)
+        header["Authorization"] = f"Bearer {self.access_token}"
+        response = requests.post(self.path_release.format(self.endpoint, payment.transaction_id), \
+                                 headers=header)
         response.raise_for_status()
 
     def refund(self, payment, amount=None):
@@ -186,12 +218,13 @@ class PaydirektProvider(BasicProvider):
             amount = payment.total
         self.check_and_update_token()
         header = PaydirektProvider.header_default.copy()
-        header.update(("Authorization", f"Bearer {self.access_token}"))
+        header["Authorization"] = f"Bearer {self.access_token}"
         body = {
             "amount": amount,
-            "callbackUrlStatusUpdates", self.get_return_url(payment)
+            "callbackUrlStatusUpdates": self.get_return_url(payment)
         }
-        response = request.post(f"/api/checkout/{self.api_version}/checkouts/{payment.transaction_id}/refunds", data=body, header=header)
+        response = requests.post(self.path_refund.format(self.endpoint, payment.transaction_id), \
+                                 json=body, headers=header)
         response.raise_for_status()
-        ob = response.json()
-        return ob["amount"]
+        json_response = response.json()
+        return json_response["amount"]
--- payments/paydirekt/test_paydirekt.py
+++ payments/paydirekt/test_paydirekt.py
@@ -13,8 +13,15 @@ except ImportError:
 from . import PaydirektProvider
 from .. import FraudStatus, PaymentError, PaymentStatus, RedirectNeeded
 
-VARIANT = 'dummy-3ds'
+VARIANT = 'paydirekt'
+API_KEY = '5a4dae68-2715-4b1e-8bb2-2c2dbe9255f6'
+SECRET = '123abc'
 
+PROCESS_DATA = {
+  "checkoutId" : "64e0bd1f-c3a3-47e1-aaff-75e690c062f8",
+  "merchantOrderReferenceNumber" : "order-A12223412",
+  "checkoutStatus" : "APPROVED"
+}
 
 class Payment(object):
     id = 1
@@ -40,122 +47,15 @@ class Payment(object):
         self.fraud_status = fraud_status
 
 
-class TestDummy3DSProvider(TestCase):
+class TestPaydirektProvider(TestCase):
 
     def setUp(self):
         self.payment = Payment()
+        self.provider = PaydirektProvider(API_KEY, SECRET)
 
-    def test_process_data_supports_verification_result(self):
-        provider = DummyProvider()
-        verification_status = PaymentStatus.CONFIRMED
+    def test_process_data_works(self):
         request = MagicMock()
-        request.GET = {'verification_result': verification_status}
-        response = provider.process_data(self.payment, request)
+        request.json = lambda : PROCESS_DATA.copy()
+        response = self.provider.process_data(self.payment, request)
         self.assertEqual(self.payment.status, verification_status)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], self.payment.get_success_url())
-
-    def test_process_data_redirects_to_success_on_payment_success(self):
-        self.payment.status = PaymentStatus.PREAUTH
-        provider = DummyProvider()
-        request = MagicMock()
-        request.GET = {}
-        response = provider.process_data(self.payment, request)
-        self.assertEqual(response['location'], self.payment.get_success_url())
-
-    def test_process_data_redirects_to_failure_on_payment_failure(self):
-        self.payment.status = PaymentStatus.REJECTED
-        provider = DummyProvider()
-        request = MagicMock()
-        request.GET = {}
-        response = provider.process_data(self.payment, request)
-        self.assertEqual(response['location'], self.payment.get_failure_url())
-
-    def test_provider_supports_non_3ds_transactions(self):
-        provider = DummyProvider()
-        data = {
-            'status': PaymentStatus.PREAUTH,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': '3ds-disabled',
-            'verification_result': ''
-        }
-        with self.assertRaises(RedirectNeeded) as exc:
-            provider.get_form(self.payment, data)
-            self.assertEqual(exc.args[0], self.payment.get_success_url())
-
-    def test_provider_raises_verification_result_needed_on_success(self):
-        provider = DummyProvider()
-        data = {
-            'status': PaymentStatus.WAITING,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': '3ds-redirect'}
-
-        form = provider.get_form(self.payment, data)
-        self.assertFalse(form.is_valid())
-
-    def test_provider_supports_3ds_redirect(self):
-        provider = DummyProvider()
-        verification_result = PaymentStatus.CONFIRMED
-        data = {
-            'status': PaymentStatus.WAITING,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': '3ds-redirect',
-            'verification_result': verification_result
-        }
-        params = urlencode({'verification_result': verification_result})
-        expected_redirect = '%s?%s' % (self.payment.get_process_url(), params)
-
-        with self.assertRaises(RedirectNeeded) as exc:
-            provider.get_form(self.payment, data)
-            self.assertEqual(exc.args[0], expected_redirect)
-
-    def test_provider_supports_gateway_failure(self):
-        provider = DummyProvider()
-        data = {
-            'status': PaymentStatus.WAITING,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': 'failure',
-            'verification_result': ''
-        }
-        with self.assertRaises(URLError):
-            provider.get_form(self.payment, data)
-
-    def test_provider_raises_redirect_needed_on_success(self):
-        provider = DummyProvider()
-        data = {
-            'status': PaymentStatus.PREAUTH,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': '3ds-disabled',
-            'verification_result': ''
-        }
-        with self.assertRaises(RedirectNeeded) as exc:
-            provider.get_form(self.payment, data)
-            self.assertEqual(exc.args[0], self.payment.get_success_url())
-
-    def test_provider_raises_redirect_needed_on_failure(self):
-        provider = DummyProvider()
-        data = {
-            'status': PaymentStatus.ERROR,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': '3ds-disabled',
-            'verification_result': ''
-        }
-        with self.assertRaises(RedirectNeeded) as exc:
-            provider.get_form(self.payment, data)
-            self.assertEqual(exc.args[0], self.payment.get_failure_url())
-
-    def test_provider_raises_payment_error(self):
-        provider = DummyProvider()
-        data = {
-            'status': PaymentStatus.PREAUTH,
-            'fraud_status': FraudStatus.UNKNOWN,
-            'gateway_response': 'payment-error',
-            'verification_result': ''
-        }
-        with self.assertRaises(PaymentError):
-            provider.get_form(self.payment, data)
-
-    def test_provider_switches_payment_status_on_get_form(self):
-        provider = DummyProvider()
-        form = provider.get_form(self.payment, data={})
-        self.assertEqual(self.payment.status, PaymentStatus.INPUT)
+        self.assertEqual(response.status_code, 200)
