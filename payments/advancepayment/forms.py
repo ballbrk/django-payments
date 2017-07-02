56b1bca350d186cab2bef08b9c69fca0c6cc4337
alex <devkral@web.de>
2017-07-02 22:20:34 +0200

    fix more typos

--- payments/advancepayment/forms.py
+++ payments/advancepayment/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class IBANBankingForm(forms.Form):
     # only shown, return is ignored
-    orderid = forms.Charfield(disabled=True)
-    iban = forms.Charfield(disabled=True)
-    bic = forms.Charfield(disabled=True)
+    orderid = forms.CharField(disabled=True)
+    iban = forms.CharField(disabled=True)
+    bic = forms.CharField(disabled=True)
